b4792eaac2f95d0cd147af5643da038e7024ce4f
caioesteves10 <caioesteves10@gmail.com>
2026-08-11 14:40:29 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - NUNCA intercepta o tráfego do Firestore: ele tem o próprio mecanismo offline
    Ao publicar uma versão nova, troque o número em VERSAO abaixo.            */
 
-const VERSAO = "proto-v19";
+const VERSAO = "proto-v20";
 const CACHE_APP = "app-" + VERSAO;
 const CACHE_EXT = "ext-" + VERSAO;
 
